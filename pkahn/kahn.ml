62b493309e872482cb182bc1083197d72c42c976
LaCacahueteTueuse <sdf@hotmail.com>
2013-05-17 19:05:50 +0200

    L'empire du milieu

---
+++ pkahn/kahn.ml
@@ -0,0 +1,34 @@
+open Unix
+
+type 'a process = (unit -> 'a)
+
+type 'a in_port = in_channel
+type 'a out_port = out_channel
+
+let new_channel () = 
+  let (in_pipe,out_pipe) = pipe () in
+  (in_channel_of_descr in_pipe, out_channel_of_descr out_pipe)
+
+let put x out_chan () = 
+  Marshal.to_channel out_chan x [Marshal.Closures];
+  flush out_chan
+
+let get in_chan () = 
+  Marshal.from_channel in_chan
+
+let fork f = 
+begin match fork () with
+  | 0 -> f (); exit 0
+  | n -> n
+end 
+
+let doco liste () = 
+  let pid = List.map fork liste in
+  List.iter (fun x -> ignore (waitpid [] x)) pid   
+
+let return x () = x
+
+let bind p q = q (p ())
+
+let run f = f ()  
+
